d020229655eb5d2467513d79c2f48fe6d32d2df5
aslammiya <aslammiya5257@gmail.com>
2024-09-09 14:14:47 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,11 @@
 echo "BUILD START"
 # python3 -m venv ./env
 # source ./env/bin/activate
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Run migrations (optional, depending on the project)
 # python manage.py migrate
 
 # Collect static files (optional, if you have static files)
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 echo "BUILD END"
