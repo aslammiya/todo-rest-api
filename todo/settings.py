3ba82df74f9b81e94976b7760789a99dcd99d40a
aslammiya <aslammiya5257@gmail.com>
2024-09-09 19:41:35 +0530

    d

--- todo/settings.py
+++ todo/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,6 +113,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'home', 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA_URLS = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
